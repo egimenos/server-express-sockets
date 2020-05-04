9e2e081e33f06d441f86b576189e294fc93c8292
EgimenoS <ergisan@gmail.com>
2020-05-04 13:47:11 +0200

    added sockets config to server

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,31 @@
 import express from "express";
 import { SERVER_PORT } from "../globals/environment";
+import socketIO from "socket.io";
+import http from "http";
 
 export default class Server {
   public app: express.Application;
   public port: number;
+  public io: socketIO.Server;
+  private httpServer: http.Server;
 
   constructor() {
     this.app = express();
     this.port = SERVER_PORT;
+
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIO(this.httpServer);
+    this.listenSockets();
+  }
+
+  private listenSockets() {
+    console.log("listening sockets");
+    this.io.on("connection", (client) => {
+      console.log("new client connected");
+    });
   }
 
   start(callback: any) {
-    this.app.listen(this.port, callback);
+    this.httpServer.listen(this.port, callback);
   }
 }
